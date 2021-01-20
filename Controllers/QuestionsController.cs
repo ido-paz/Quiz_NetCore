5d204a762e9b278250bdd1abc299c1e003569642
ido <ido_p_a@hotmail.com>
2021-01-20 20:19:49 +0200

    resolves #2

---
+++ Controllers/QuestionsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Quiz_NetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quiz_NetCore.Controllers
+{
+    public class QuestionsController : ControllerBase
+    {
+        Quiz_DBContext DB;
+        public QuestionsController(Quiz_DBContext db) {
+            DB = db;
+        }
+
+        [Route("GetQuestions")]
+        public ActionResult GetQuestions()
+        {
+            return Ok(DB.Questions);
+        }
+        //
+        [Route("GetQuestion/{id}")]
+        public ActionResult GetQuestion(int id)
+        {
+            Question q=  DB.Questions.FirstOrDefault(q => q.ID == id);
+            if (q == null)
+                return NotFound($"question {id} not found");
+            return Ok(q);
+        }
+        //
+        [Route("AddQuestion")]
+        [HttpPost]
+        public ActionResult AddQuestion(Question question2Add)
+        {
+            if(DB.Questions.Find(q => q.Text.Trim() == question2Add.Text.Trim()) == null)
+            {
+                DB.Questions.Add(question2Add);
+                return Created(Request.Path, question2Add.ID);
+            }
+            return Conflict("Question allready exists");
+        }
+        //
+        [Route("RemoveQuestion")]
+        [HttpDelete]
+        public ActionResult RemoveQuestion(int id)
+        {
+            Question q = DB.Questions.FirstOrDefault(q => q.ID == id);
+            if(DB.Questions.Remove(q))
+                return Ok($"question {id} removed");
+            else
+                return NotFound($"question {id} not found");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,8 @@ namespace Quiz_NetCore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSingleton<Quiz_DBContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +33,8 @@ namespace Quiz_NetCore
 
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapControllerRoute("default","/",)
+                endpoints.MapControllers();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
